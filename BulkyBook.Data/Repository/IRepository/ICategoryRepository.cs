91387e2fe2e546a5f1a43213e7d6e35ca1b5e8dd
kasib-94 <jakczarski@gmail.com>
2022-05-29 11:13:53 +0200

    attaching main project, adding Respository Pattern

---
+++ BulkyBook.Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.Data.IRepository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    void Update(Category obj);
+    void Save();
+}
